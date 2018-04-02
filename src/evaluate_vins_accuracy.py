Fix error computation

src/evaluate_vins_accuracy.py
@@ -175,12 +175,13 @@ def estimate(ts_vins, vins_poses, ts_mocap, mocap_poses):
         ts_mocap - Timestamps for Mocap poses [nm]
         vins_poses - Vins poses [7xnv]
         mocap_poses - Mocap poses [7xnm]
-    Return matrices [2x7] where first row is Mocap to VINS and second row
-    is IMU to Marker. Pose = [x,y,z,Qx,Qy,Qz,Qw]
+    Return matrices [1x14] where first 7 elements are Mocap to VINS and second 7
+    are IMU to Marker. Pose = [x,y,z,Qx,Qy,Qz,Qw]
     """
     vins_interp = interpolatePoses(ts_mocap, ts_vins, vins_poses)
     cons = ({'type': 'eq', 'fun': quatConstraint1},
             {'type': 'eq', 'fun': quatConstraint2})
-    res = opt.minimize(cost, np.zeros(14), args=(vins_interp, mocap_poses),
+    x0 = np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1])
+    res = opt.minimize(cost, x0, args=(vins_interp, mocap_poses),
                        constraints=cons, method='SLSQP')
     return res.x

src/frontend.py
@@ -32,7 +32,7 @@ def plot_error():
     for i in range(0, 7):
         titles = ['error in translation in x','error in translation in y', 'error in translation in z','error in orientation in x', 'error in orientation in y', 'error in orientation in z','error in orientation in w']
         plt.figure(i)
-        plt.plot(vins_data[:, 0], error[i,:], 'r')
+        plt.plot(ts_mocap, error[i,:], 'r')
         plt.title(titles[i])
     plt.show()
 
@@ -48,9 +48,10 @@ mocap_poses = np.transpose(mocap_poses)
 
 # Find estimates
 estimates = vins_acc.estimate(ts_vins, vins_poses, ts_mocap, mocap_poses)
+vins_interp = vins_acc.interpolatePoses(ts_mocap, ts_vins, vins_poses)
 
 # Find and plot error
-vins_in_mocap = vins_acc.transformVINSToMocap(estimates, vins_poses.T)
-error = mocap_poses[:,:vins_in_mocap.shape[1]] - vins_in_mocap
+vins_in_mocap = vins_acc.transformVINSToMocap(estimates, vins_interp)
+error = mocap_poses - vins_in_mocap
 
 plot_error()